d660bbd7427489697d4d8b24cf61e983c44d542c
AymericHenouille <aymeric.henouille@gmail.com>
2024-01-16 23:04:11 +0100

    inital commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { linear } from 'svelte/easing';
+  import { tweened } from 'svelte/motion';
+
+  const left = tweened(0, {
+		duration: 400,
+		easing: linear,
+	});
+
+  setInterval(() => {
+    left.update(n => (++n) % 100);
+  }, 400);
+</script>
+
+<main>
+  <div class="line" style="left: {$left}%"></div>
+</main>
+
+<style>
+  main {
+    height: 100%;
+    width: 100%;
+  }
+
+  .line {
+    width: 5px;
+    height: 100%;
+    background-color: #368974;
+    position: fixed;
+  }
+</style>
